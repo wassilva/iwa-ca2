c60885ecc89108d9d426954894e68bb0489b09ec
wassilva <wassilva@gmail.com>
2021-04-05 19:54:46 -0300

    Route listAll vehicles

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ mongoose.connect(DATABASE_URL, {
 server.use(express.static("public"))
 server.use(express.json());
 
+server.get('/', async (request, response) => {
+  const vehicles = await Vehicle.find({});
+  return response.json(vehicles);
+});
+
 server.post('/vehicle/store', async (request, response) => {
   const { type, manufacturer, model, vehicle_year, value, quantity } = request.body;
 
